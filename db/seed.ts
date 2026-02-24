527d61330ba6ce72aec642b4075cb36f40b9e305
jeromeabel <j.abel@allo-media.fr>
2026-02-24 18:05:28 +0100

    chore: move to php/mysql

--- db/seed.ts
+++
@@ -1,9 +0,0 @@
-import { db, Vote } from 'astro:db';
-
-export default async function seed() {
-  await db.insert(Vote).values([
-    { comicId: '001', visitorId: 'visitor-aaa' },
-    { comicId: '001', visitorId: 'visitor-bbb' },
-    { comicId: '002', visitorId: 'visitor-aaa' },
-  ]);
-}
